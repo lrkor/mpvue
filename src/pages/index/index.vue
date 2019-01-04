377b2c66fba83b274111c64bcd0d67b5defc219b
lrkor <1055409319@qq.com>
2019-01-04 14:42:36 +0800

    mpVue

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -25,39 +25,5 @@
 </script>
 
 <style scoped>
-  .userinfo {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
 
-  .userinfo-avatar {
-    width: 128 rpx;
-    height: 128 rpx;
-    margin: 20 rpx;
-    border-radius: 50%;
-  }
-
-  .userinfo-nickname {
-    color: #aaa;
-  }
-
-  .usermotto {
-    margin-top: 150px;
-  }
-
-  .form-control {
-    display: block;
-    padding: 0 12px;
-    margin-bottom: 5px;
-    border: 1px solid #ccc;
-  }
-
-  .counter {
-    display: inline-block;
-    margin: 10px auto;
-    padding: 5px 10px;
-    color: blue;
-    border: 1px solid blue;
-  }
 </style>
